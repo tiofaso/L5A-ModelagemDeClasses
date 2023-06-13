c93c546084915762bf387267b5087d0a70257fee
tiofaso <fabio.vaz@zup.com.br>
2023-06-13 18:49:39 -0300

    :bug: Construtor havia sido criado errado e prog. melhorado

--- src/carro/Carro.java
+++ src/carro/Carro.java
@@ -1,16 +1,45 @@
 package carro;
 
 public class Carro {
-    Integer carroPortas;
-    String carroModelo;
-    String carroMarca;
+    int carroPortas;
+    int carroCarroceria;
+    int carroMarca;
     double carroPotencia;
-    Integer carroCarroceria;
+
+    String carroModelo;
 
     public void ligaCarro(){System.out.println("Vrum! VRUUUUUM!");}
 
-    public void Carro(String carroMarca, String carroModelo){
-        System.out.println("O carro ideal para você é: " + carroMarca + " " + carroModelo + " com motor " + carroPotencia);
-        ligaCarro();
+    public void CarroEscolhido(){
+        if(carroPortas == 1 && carroCarroceria == 1 && carroMarca == 1){this.carroModelo = "Ka"; this.carroPotencia = 1.0 ;}
+        else if(carroPortas == 1 && carroCarroceria == 1 && carroMarca == 2){this.carroModelo = "Palio"; this.carroPotencia = 1.0 ;}
+        else if(carroPortas == 1 && carroCarroceria == 1 && carroMarca == 3){this.carroModelo = "Gol"; this.carroPotencia = 1.0 ;}
+
+        else if(carroPortas == 2 && carroCarroceria == 2 && carroMarca == 1){this.carroModelo = "Ka+"; this.carroPotencia = 1.0;}
+        else if(carroPortas == 2 && carroCarroceria == 2 && carroMarca == 2){this.carroModelo = "Siena"; this.carroPotencia = 1.0;}
+        else if(carroPortas == 2 && carroCarroceria == 2 && carroMarca == 3){this.carroModelo = "Voyage"; this.carroPotencia = 1.0 ;}
+
+        else if(carroPortas == 2 && carroCarroceria == 3 && carroMarca == 1){this.carroModelo = "Ecosport";}
+        else if(carroPortas == 2 && carroCarroceria == 3 && carroMarca == 2){this.carroModelo = "Pulse"; this.carroPotencia = 1.6 ;}
+        else if(carroPortas == 2 && carroCarroceria == 3 && carroMarca == 3){this.carroModelo = "T-cross";}
+
+        else if(carroPortas == 2 && carroCarroceria == 1 && carroMarca == 1){this.carroModelo = "Fiesta"; }
+        else if(carroPortas == 2 && carroCarroceria == 1 && carroMarca == 2){this.carroModelo = "Argo"; this.carroPotencia = 1.3 ;}
+        else if(carroPortas == 2 && carroCarroceria == 1 && carroMarca == 3){this.carroModelo = "Golf"; }
+
+        else {this.carroModelo = "666";}
+
+        if(this.carroModelo != "666") {
+            System.out.println("O carro ideal para você é: " + this.carroMarca + " " + this.carroModelo + " com motor " + this.carroPotencia);
+            ligaCarro();
+        }else{System.out.println("Não achamos um carro ideal para você... :(");}
+
+    }
+
+    public Carro(int carroPortas, int carroCarroceria, int carroMarca) {
+        this.carroPortas = carroPortas;
+        this.carroCarroceria = carroCarroceria;
+        this.carroMarca = carroMarca;
+        this.carroPotencia = 2.0;
     }
 }
--- src/carro/Main.java
+++ src/carro/Main.java
@@ -4,51 +4,44 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
+        System.out.println("----------------------------------------------------");
         System.out.println("Vamos configurar o seu próximo carro?");
         System.out.println("Digite alguns dados abaixo que vou te mostrar opções");
+        System.out.println("----------------------------------------------------\n");
+
+        int sairLoja = 2; //flag para sair da configuração
 
         //Inseridos dados do usuário
         Scanner entrada = new Scanner(System.in);
 
-        Carro meuCarro = new Carro(); //Instanciando o carro
+        while(sairLoja == 2) {
+            //Escolhendo quantas portas
+            System.out.println("Quantas portas você deseja?");
+            System.out.println("1 - duas portas");
+            System.out.println("2 - quatro portas");
+            int carroPortas = entrada.nextInt();
 
-        //Potência padrão
-        meuCarro.carroPotencia = 2.0;
+            //Escolhendo um tipo de carroceira
+            System.out.println("Escolha um tipo de carroceria:");
+            System.out.println("1 - hatch");
+            System.out.println("2 - sedan");
+            System.out.println("3 - SUV");
+            int carroCarroceria = entrada.nextInt();
 
-        System.out.println("Quantas portas você deseja?");
-        meuCarro.carroPortas = entrada.nextInt();
+            //Escolhendo marca de carro
+            System.out.println("Escolha uma marca:");
+            System.out.println("1 - Ford");
+            System.out.println("2 - Fiat");
+            System.out.println("3 - Volkswagen");
+            int carroMarca = entrada.nextInt();
 
-        System.out.println("Escolha um tipo de uso:");
-        System.out.println("1 - urbano");
-        System.out.println("2 - familiar");
-        System.out.println("3 - carga");
+            Carro meuCarro = new Carro(carroPortas, carroCarroceria, carroMarca);
 
-        meuCarro.carroCarroceria = entrada.nextInt();
+            meuCarro.CarroEscolhido();
 
-        //Definindo características do carro
-        if(meuCarro.carroPortas == 2 && meuCarro.carroCarroceria == 1){//Hatch
-            meuCarro.carroMarca = "Volkswagen";
-            meuCarro.carroPotencia = 1.0;
-            meuCarro.carroModelo = "Gol";
-        }else if(meuCarro.carroPortas == 2 && meuCarro.carroCarroceria == 3){//Picape
-            meuCarro.carroMarca = "Ford";
-            meuCarro.carroModelo = "Ranger";
-        }else if(meuCarro.carroPortas > 2 && meuCarro.carroCarroceria == 1){//Hatch médio
-            meuCarro.carroMarca = "Ford";
-            meuCarro.carroModelo = "Focus";
-        }else if(meuCarro.carroPortas > 2 && meuCarro.carroCarroceria == 2){//Minivan
-            meuCarro.carroMarca = "Fiat";
-            meuCarro.carroModelo = "Doblò";
-            meuCarro.carroPotencia = 1.8;
-        }else if(meuCarro.carroPortas > 2 && meuCarro.carroCarroceria == 3) {//Picape média
-            meuCarro.carroMarca = "Toyota";
-            meuCarro.carroModelo = "Hilux";
-            meuCarro.carroPotencia = 3.0;
-        }else{
-            meuCarro.carroMarca = "qualquer marca";
-            meuCarro.carroModelo = "e qualquer modelo";
-        } //fim if
-        meuCarro.Carro(meuCarro.carroMarca, meuCarro.carroModelo);
-
+            //Verificando se o usuário quer sair da configuração
+            System.out.println("\nDeseja encerrar a configuração do carro? 1 - SIM | 2 - NÃO");
+            sairLoja = entrada.nextInt();
+        }// fim while
     }
 }
